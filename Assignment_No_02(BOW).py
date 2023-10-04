dbc3bbbc4f9c6d4b54f4da5a5fdab8393e61d4a3
GHF2002 <asmita.hon2002@gmail.com>
2023-10-04 13:14:40 +0530

    BOW and TFIDF done on 04/10/2023

--- Assignment_No_02(BOW).py
+++ Assignment_No_02(BOW).py
@@ -1,30 +1,58 @@
-#Assignment No.02##
-#Name:Hon Asmita R. 
-##Roll No:71
-#Batch:B4
-#Title:"Assignment based on bag of words"
-
+#Assignment no : 2
+#Name : Asmita Hon 
+#Batch : B4
+#Roll no : 71
+#Title : Natural Language Processing (NLP) using Gensim
 
 import gensim
 from gensim import corpora
-text1 = ["The food is excellent but the service can be better",
-        "The food is always delicious and loved the service",
-        "The food was mediocre and the service was terrible"]
+from gensim.utils import simple_preprocess
+from gensim import corpora
+
+text1 = ["My name is Asmita Hon.", 
+   "I am from Sanjivani College of Engineering.", 
+   "Asmita is from kopargaon. "]
+
+
 tokens1 = [[item for item in line.split()] for line in text1]
 g_dict1 = corpora.Dictionary(tokens1)
-# Creating Bag Of Words
+
+print("The dictionary has: " +str(len(g_dict1)) + " tokens\n")
+print(g_dict1.token2id)
+
 g_bow =[g_dict1.doc2bow(token, allow_update = True) for token in tokens1]
 print("Bag of Words : ", g_bow)
 
-# Creating TF-IDF
+
+print("\n--------------------------------------------------------------------------------------------------------------------------")
+print("-----TFIDF------------------------------------------------------------------------------------------------------------------")
+text = ["My name is Asmita Hon.", 
+   "I am from Sanjivani College of Engineering.", 
+   "Asmita is from kopargaon. "]
+
+g_dict = corpora.Dictionary([simple_preprocess(line) for line in text])
+g_bow = [g_dict.doc2bow(simple_preprocess(line)) for line in text]
+
 print("Dictionary : ")
 for item in g_bow:
-    print([[g_dict1[id], freq] for id, freq in item])
+    print([[g_dict[id], freq] for id, freq in item])
+
+# g_tfidf = models.TfidfModel(g_bow, smartirs='ntc')
+
+# print("TF-IDF Vector:")
+# for item in g_tfidf[g_bow]:
+#     print([[g_dict[id], np.around(freq, decimals=2)] for id, freq in item])
 
 
-"""OUTPUT"""
-# Bag of Words :  [[(0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1), (9, 1)], [(0, 1), (6, 1), (7, 1), (8, 1), (9, 1), (10, 1), (11, 1), (12, 1), (13, 1)], [(0, 1), (6, 1), (8, 1), (9, 1), (11, 1), (14, 1), (15, 1), (16, 2)]]
+#OUTPUT
+# The dictionary has: 13 tokens
+
+# {'Asmita': 0, 'My': 1, 'Hon.': 2, 'is': 3, 'name': 4, 'College': 5, 'Engineering.': 6, 'I': 7, 'Sanjivani': 8, 'am': 9, 'from': 10, 'of': 11, 'Junner.': 12}
+# Bag of Words :  [[(0, 1), (1, 1), (2, 1), (3, 1), (4, 1)], [(5, 1), (6, 1), (7, 1), (8, 1), (9, 1), (10, 1), (11, 1)], [(0, 1), (3, 1), (10, 1), (12, 1)]]
+
+# --------------------------------------------------------------------------------------------------------------------------
+# -----TFIDF------------------------------------------------------------------------------------------------------------------
 # Dictionary : 
-# [['The', 1], ['be', 1], ['better', 1], ['but', 1], ['can', 1], ['excellent', 1], ['food', 1], ['is', 1], ['service', 1], ['the', 1]]
-# [['The', 1], ['food', 1], ['is', 1], ['service', 1], ['the', 1], ['always', 1], ['and', 1], ['delicious', 1], ['loved', 1]]
-# [['The', 1], ['food', 1], ['service', 1], ['the', 1], ['and', 1], ['mediocre', 1], ['terrible', 1], ['was', 2]]
+# [['asmita', 1], ['is', 1], ['my', 1], ['name', 1], ['hon', 1]]
+# [['am', 1], ['college', 1], ['engineering', 1], ['from', 1], ['of', 1], ['sanjivani', 1]]
+# [['asmita', 1], ['is', 1], ['from', 1], ['Kopargaon', 1]]
